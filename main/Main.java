821bfb6cab9e1a4ff051636b9057a6373e4f9208
SahaiAnshul <86490336+SahaiAnshul@users.noreply.github.com>
2024-12-30 16:58:39 +0530

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+import controllers.MusicController;
+
+public class Main {
+    public static void main(String[] args) {
+        MusicController controller = new MusicController();
+        controller.startApplication();
+    }
+}
